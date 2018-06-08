9c8a4424c8516ec7eb66cca5cde5bdce3d37ef09
Victor Vrantchan <vrancean+github@gmail.com>
2018-06-08 12:28:59 -0700

    with_mdm

--- cmd/extension/platform_plugins_darwin.go
+++ cmd/extension/platform_plugins_darwin.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"os"
+
+	"github.com/go-kit/kit/log"
 	osquery "github.com/kolide/osquery-go"
 
+	"github.com/kolide/go-extension-tutorial/pkg/mdm"
 	"github.com/kolide/go-extension-tutorial/pkg/spotlight"
 )
 
 func registerPlatformPlugins(server *osquery.ExtensionManagerServer) {
+	logger := log.NewLogfmtLogger(os.Stderr)
+	mdmPlugin := mdm.MDMInfo(logger)
+	server.RegisterPlugin(mdmPlugin)
+
 	// register the macOS spotlight plugin.
 	server.RegisterPlugin(spotlight.New())
 }
